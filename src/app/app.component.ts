c164aa504b24dcc34709f66eb78ce41dd9d00131
Ragnarok <Deathmetalldrummer@gmail.com>
2021-01-03 23:50:05 +0600

    Socket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'api';
+  socket: any;
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.socket = io('http://localhost:4000', {
+      transports: ['websocket']
+    });
+    this.socket.on('connect', () => {
+      console.log('connected');
+    });
+    this.socket.on('fuckYOU', (socket) => {
+      console.log('>>>>>>>>>', socket);
+    });
+  }
+  methodX(x): void {
+    console.log('socket emitted');
+    this.socket.emit('fuck', {fuck: 'socket'});
+  }
 }
